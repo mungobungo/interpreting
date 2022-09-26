485f09032b79cafbceb9ddef2c7a57d4edcb5268
oleg <oleg.taykalo@gmail.com>
2022-09-26 23:06:57 +0200

    added unparse tests

--- src/main/kotlin/Domain.kt
+++ src/main/kotlin/Domain.kt
@@ -1,9 +1,14 @@
 interface Expression{
     fun eval():Expression
+    fun unparse():String
 }
 data class EInt(val value:Int):Expression {
     override fun eval(): Expression {
         return this
+    }
+
+    override fun unparse(): String {
+        return value.toString()
     }
 }
 
@@ -17,6 +22,10 @@ data class EMul(val a:Expression, val b:Expression): Expression {
         val right = b.eval() as EInt
         return EInt(left.value * right.value)
     }
+
+    override fun unparse(): String {
+        return "[mul, ${a.unparse()}, ${b.unparse()}]".format()
+    }
 }
 
 data class EAdd(val a:Expression, val b:Expression): Expression {
@@ -25,6 +34,10 @@ data class EAdd(val a:Expression, val b:Expression): Expression {
         val right = b.eval() as EInt
         return EInt(left.value + right.value)
 
+    }
+
+    override fun unparse(): String {
+        return "[add, ${a.unparse()}, ${b.unparse()}]"
     }
 }
 
--- src/test/kotlin/ParserKtTest.kt
+++ src/test/kotlin/ParserKtTest.kt
@@ -15,11 +15,11 @@ internal class ParserKtTest {
     fun tearDown() {
     }
 
-    @Test()
+    @Test
     fun testSimpleInt(){
         assertEquals(EInt(42), parse("42"))
     }
-    @Test()
+    @Test
     fun testSimpleAdd(){
         assertEquals(EAdd(EInt(3), EInt(5)), parse("[add, 3, 5]"))
     }
@@ -44,5 +44,49 @@ internal class ParserKtTest {
          assertEquals(
             EMul(EAdd(EInt(22), EInt(11)), EInt(44)),
             parse("[mul, [add, 22, 11], 44]"))
+    }
+
+    @Test
+    fun testSimpleIntUnparse(){
+        assertEquals("42",EInt(42).unparse())
+        assertEquals("42", parse("42").unparse())
+    }
+
+    @Test
+    fun testSimpleAddUnparse(){
+        val yaml = "[add, 3, 5]"
+        assertEquals(yaml, EAdd(EInt(3), EInt(5)).unparse())
+        assertEquals(yaml, parse(yaml).unparse())
+    }
+    @Test
+    fun testComplicatedAddUnparse(){
+        val yaml = "[add, [add, 3, 5], 42]"
+        assertEquals(
+            yaml,
+            EAdd(EAdd(EInt(3), EInt(5)), EInt(42)).unparse())
+        assertEquals(yaml, parse(yaml).unparse())
+    }
+    @Test
+    fun testSimpleMultiplicationUnparse(){
+        val yaml = "[mul, 11, 33]"
+
+        assertEquals(yaml, EMul(EInt(11), EInt(33)).unparse())
+        assertEquals(yaml, parse(yaml).unparse())
+    }
+    @Test
+    fun testComplicatedMultiplicationUnparse(){
+        val yaml = "[mul, [mul, 3, 5], 42]"
+        assertEquals(
+            yaml,
+            EMul(EMul(EInt(3), EInt(5)), EInt(42)).unparse())
+        assertEquals(yaml, parse(yaml).unparse())
+    }
+    @Test
+    fun testMulAndAddUnparse(){
+        val yaml = "[mul, [add, 22, 11], 44]"
+        assertEquals(
+            yaml,
+            EMul(EAdd(EInt(22), EInt(11)), EInt(44)).unparse())
+        assertEquals(yaml, parse(yaml).unparse())
     }
 }
