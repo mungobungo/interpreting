1b0cc84cb458398618778aa9b40411299e4b09e2
oleg <oleg.taykalo@gmail.com>
2022-09-26 20:33:14 +0200

    extracted evaluation to the expressions

--- src/main/kotlin/Domain.kt
+++ src/main/kotlin/Domain.kt
@@ -1,18 +1,26 @@
-interface Expression
-data class EInt(val value:Int):Expression
-data class EMul(val a:Expression, val b:Expression): Expression
-data class EAdd(val a:Expression, val b:Expression): Expression
-
-fun eval( expression:Expression): Expression{
-    if(expression is EAdd){
-        val left: EInt = eval(expression.a) as EInt
-        val right: EInt  = eval(expression.b) as EInt
-        return EInt(left.value + right.value)
+interface Expression{
+    fun eval():Expression
+}
+data class EInt(val value:Int):Expression {
+    override fun eval(): Expression {
+        return this
     }
-    if(expression is EMul){
-        val left: EInt = eval(expression.a) as EInt
-        val right: EInt  = eval(expression.b) as EInt
+}
+
+data class EMul(val a:Expression, val b:Expression): Expression {
+    override fun eval(): Expression {
+        val left = a.eval() as EInt
+        val right = b.eval() as EInt
         return EInt(left.value * right.value)
     }
-    return expression
 }
+
+data class EAdd(val a:Expression, val b:Expression): Expression {
+    override fun eval(): Expression {
+        val left = a.eval() as EInt
+        val right = b.eval() as EInt
+        return EInt(left.value + right.value)
+
+    }
+}
+
--- src/test/kotlin/DomainKtTest.kt
+++ src/test/kotlin/DomainKtTest.kt
@@ -16,37 +16,37 @@ internal class DomainKtTest {
 
     @Test
     fun testSimpleInteger() {
-        assertEquals(EInt(5), eval(EInt(5)))
-        assertEquals(EInt(-5), eval(EInt(-5)))
-        assertEquals(EInt(0), eval(EInt(0)))
+        assertEquals(EInt(5), EInt(5).eval())
+        assertEquals(EInt(-5),(EInt(-5)).eval())
+        assertEquals(EInt(0), EInt(0).eval())
     }
 
     @Test
     fun testSimpleAddition() {
-        assertEquals(EInt(5), eval(EAdd(EInt(5), EInt(0))))
-        assertEquals(EInt(5), eval(EAdd(EInt(0), EInt(5))))
-        assertEquals(EInt(10), eval(EAdd(EInt(7), EInt(3))))
-        assertEquals(EInt(7), eval(EAdd(EInt(-3), EInt(10))))
-        assertEquals(EInt(6), eval(EAdd(EInt(10), EInt(-4))))
+        assertEquals(EInt(5), EAdd(EInt(5), EInt(0)).eval())
+        assertEquals(EInt(5), (EAdd(EInt(0), EInt(5))).eval())
+        assertEquals(EInt(10),(EAdd(EInt(7), EInt(3))).eval())
+        assertEquals(EInt(7), (EAdd(EInt(-3), EInt(10))).eval())
+        assertEquals(EInt(6), (EAdd(EInt(10), EInt(-4))).eval())
     }
 
     @Test
     fun testComplexAddition() {
-        assertEquals(EInt(6), eval(EAdd(EInt(1), EAdd(EInt(2), EInt(3)))))
+        assertEquals(EInt(6), (EAdd(EInt(1), EAdd(EInt(2), EInt(3)))).eval())
     }
 
     @Test
     fun testSimpleMultiplication() {
-        assertEquals(EInt(0), eval(EMul(EInt(0), EInt(42))))
-        assertEquals(EInt(0), eval(EMul(EInt(42), EInt(0))))
-        assertEquals(EInt(42), eval(EMul(EInt(1), EInt(42))))
-        assertEquals(EInt(42), eval(EMul(EInt(42), EInt(1))))
-        assertEquals(EInt(4), eval(EMul(EInt(2), EInt(2))))
+        assertEquals(EInt(0), (EMul(EInt(0), EInt(42))).eval())
+        assertEquals(EInt(0), (EMul(EInt(42), EInt(0))).eval())
+        assertEquals(EInt(42),(EMul(EInt(1), EInt(42))).eval())
+        assertEquals(EInt(42), (EMul(EInt(42), EInt(1))).eval())
+        assertEquals(EInt(4), (EMul(EInt(2), EInt(2))).eval())
     }
 
     @Test
     fun testComplicatedMultiplication() {
-        assertEquals(EInt(10), eval(EMul(EMul(EInt(1), EInt(5)), EInt(2))))
-        assertEquals(EInt(12), eval(EMul(EAdd(EInt(1), EInt(5)), EInt(2))))
+        assertEquals(EInt(10), (EMul(EMul(EInt(1), EInt(5)), EInt(2))).eval())
+        assertEquals(EInt(12), (EMul(EAdd(EInt(1), EInt(5)), EInt(2))).eval())
     }
 }
