9dc4f8789ade9c58040c5cb49dfaaa81ec3b95ca
oleg <oleg.taykalo@gmail.com>
2022-09-27 22:55:40 +0200

    added evaluation of parsed expressions

--- src/test/kotlin/DomainKtTest.kt
+++ src/test/kotlin/DomainKtTest.kt
@@ -57,4 +57,44 @@ internal class DomainKtTest {
         assertEquals(EInt(12), parse("[mul, [add, 1, 5], 2]").eval())
 
     }
+
+    @Test
+    fun testSimpleIntegerWithParsing() {
+        assertEquals(EInt(5), parse("5").eval())
+        assertEquals(EInt(-5),parse("-5").eval())
+        assertEquals(EInt(0), parse("0").eval())
+    }
+
+    @Test
+    fun testSimpleAdditionWithParsing() {
+        assertEquals(EInt(5), parse("[add, 5, 0]").eval())
+        assertEquals(EInt(5), parse("[add, 0, 5]").eval())
+        assertEquals(EInt(10),parse("[add, 7, 3]").eval())
+        assertEquals(EInt(7), parse("[add, -3, 10]").eval())
+        assertEquals(EInt(6), parse("[add, 10, -4]").eval())
+    }
+
+    @Test
+    fun testComplexAdditionWithParsing() {
+        assertEquals(EInt(6),  parse("[add, 1, [add, 2,3]]").eval())
+    }
+
+    @Test
+    fun testSimpleMultiplicationWithParsing() {
+        assertEquals(EInt(0), parse("[mul, 0, 42]").eval())
+        assertEquals(EInt(0), parse("[mul, 42, 0]").eval())
+        assertEquals(EInt(42),parse("[mul, 1, 42]").eval())
+        assertEquals(EInt(42), parse("[mul, 42, 1]").eval())
+        assertEquals(EInt(4), parse("[mul, 2, 2]").eval())
+    }
+
+    @Test
+    fun testComplicatedMultiplicationWithParsing() {
+        assertEquals(EInt(10), parse("[mul, 1, [mul, 5, 2]]").eval())
+        assertEquals(EInt(12), parse("[mul, 2, [add, 5, 1]]").eval())
+
+        assertEquals("10", parse("[mul, 1, [mul, 5, 2]]").eval().unparse())
+        assertEquals("12", parse("[mul, 2, [add, 5, 1]]").eval().unparse())
+    }
+
 }
