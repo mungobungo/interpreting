5c13be15f80c4cfe533f12e75c4f060298344e6c
oleg <oleg.taykalo@gmail.com>
2022-09-28 00:29:32 +0200

    added sugared expressions and evaluation of substraction

--- src/test/kotlin/DomainKtTest.kt
+++ src/test/kotlin/DomainKtTest.kt
@@ -52,49 +52,55 @@ internal class DomainKtTest {
     @Test
     fun testComplicatedExpressionWithParsing() {
         assertEquals(EInt(10), (EMul(EMul(EInt(1), EInt(5)), EInt(2))).eval())
-        assertEquals(EInt(10), parse("[mul, [mul, 1, 5], 2]").eval())
+        assertEquals(EInt(10), parse("[mul, [mul, 1, 5], 2]").desugar().eval())
         assertEquals(EInt(12), (EMul(EAdd(EInt(1), EInt(5)), EInt(2))).eval())
-        assertEquals(EInt(12), parse("[mul, [add, 1, 5], 2]").eval())
+        assertEquals(EInt(12), parse("[mul, [add, 1, 5], 2]").desugar().eval())
 
     }
 
     @Test
     fun testSimpleIntegerWithParsing() {
-        assertEquals(EInt(5), parse("5").eval())
-        assertEquals(EInt(-5),parse("-5").eval())
-        assertEquals(EInt(0), parse("0").eval())
+        assertEquals(EInt(5), parse("5").desugar().eval())
+        assertEquals(EInt(-5),parse("-5").desugar().eval())
+        assertEquals(EInt(0), parse("0").desugar().eval())
     }
 
     @Test
     fun testSimpleAdditionWithParsing() {
-        assertEquals(EInt(5), parse("[add, 5, 0]").eval())
-        assertEquals(EInt(5), parse("[add, 0, 5]").eval())
-        assertEquals(EInt(10),parse("[add, 7, 3]").eval())
-        assertEquals(EInt(7), parse("[add, -3, 10]").eval())
-        assertEquals(EInt(6), parse("[add, 10, -4]").eval())
+        assertEquals(EInt(5), parse("[add, 5, 0]").desugar().eval())
+        assertEquals(EInt(5), parse("[add, 0, 5]").desugar().eval())
+        assertEquals(EInt(10),parse("[add, 7, 3]").desugar().eval())
+        assertEquals(EInt(7), parse("[add, -3, 10]").desugar().eval())
+        assertEquals(EInt(6), parse("[add, 10, -4]").desugar().eval())
     }
 
     @Test
     fun testComplexAdditionWithParsing() {
-        assertEquals(EInt(6),  parse("[add, 1, [add, 2,3]]").eval())
+        assertEquals(EInt(6),  parse("[add, 1, [add, 2,3]]").desugar().eval())
     }
 
     @Test
     fun testSimpleMultiplicationWithParsing() {
-        assertEquals(EInt(0), parse("[mul, 0, 42]").eval())
-        assertEquals(EInt(0), parse("[mul, 42, 0]").eval())
-        assertEquals(EInt(42),parse("[mul, 1, 42]").eval())
-        assertEquals(EInt(42), parse("[mul, 42, 1]").eval())
-        assertEquals(EInt(4), parse("[mul, 2, 2]").eval())
+        assertEquals(EInt(0), parse("[mul, 0, 42]").desugar().eval())
+        assertEquals(EInt(0), parse("[mul, 42, 0]").desugar().eval())
+        assertEquals(EInt(42),parse("[mul, 1, 42]").desugar().eval())
+        assertEquals(EInt(42), parse("[mul, 42, 1]").desugar().eval())
+        assertEquals(EInt(4), parse("[mul, 2, 2]").desugar().eval())
     }
 
     @Test
     fun testComplicatedMultiplicationWithParsing() {
-        assertEquals(EInt(10), parse("[mul, 1, [mul, 5, 2]]").eval())
-        assertEquals(EInt(12), parse("[mul, 2, [add, 5, 1]]").eval())
+        assertEquals(EInt(10), parse("[mul, 1, [mul, 5, 2]]").desugar().eval())
+        assertEquals(EInt(12), parse("[mul, 2, [add, 5, 1]]").desugar().eval())
 
-        assertEquals("10", parse("[mul, 1, [mul, 5, 2]]").eval().unparse())
-        assertEquals("12", parse("[mul, 2, [add, 5, 1]]").eval().unparse())
+        assertEquals("10", parse("[mul, 1, [mul, 5, 2]]").desugar().eval().unparse())
+        assertEquals("12", parse("[mul, 2, [add, 5, 1]]").desugar().eval().unparse())
+    }
+
+    @Test
+    fun testMulAndSubEvaluationWithParsing(){
+        val yaml = "[mul, [sub, 22, 11], 44]"
+        assertEquals("484", parse(yaml).desugar().eval().unparse())
     }
 
 }
