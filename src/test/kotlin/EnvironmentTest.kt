8b4b644b0817b7e0fe5da5e512874e745891b91a
oleg <oleg.taykalo@gmail.com>
2022-09-28 23:23:59 +0200

    added environment settings

---
+++ src/test/kotlin/EnvironmentTest.kt
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class EnvironmentTest {
+
+    @Test
+    fun isDefined() {
+        val env = Environment(hashMapOf())
+        assertFalse(env.isDefined(ESymbol("x")))
+        val env2 = Environment(hashMapOf( Pair(ESymbol("x"), EInt(5))))
+        assertTrue(env2.isDefined(ESymbol("x")))
+    }
+    @Test
+    fun addBinding() {
+        val env = Environment(hashMapOf())
+        env.addBinding(ESymbol("x"), EInt(5))
+        assertTrue(env.isDefined(ESymbol("x")))
+        assertEquals(EInt(5), env.bindings[ESymbol("x")])
+        assertEquals(EInt(5), env.get(ESymbol("x")))
+
+    }
+
+
+}
